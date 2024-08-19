f33cb43d535bb5778efb96d8aa1f12be6d7a54eb
YBB-HANWOOL <118036855+YBB-space@users.noreply.github.com>
2024-08-19 19:20:03 +0900

    Delete apikey.js

--- apikey.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    Openai_api_key:"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"
-};
-
-export default config;
